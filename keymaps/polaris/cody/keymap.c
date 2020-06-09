e1d4606c93dd24df7c41f9fa5a7a8b61dae14b57
Cody Bloemhard <cblmhrd@live.nl>
2020-06-10 01:14:58 +0200

    new layout using modtap

--- keymaps/polaris/cody/keymap.c
+++ keymaps/polaris/cody/keymap.c
@@ -36,17 +36,17 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_all( /* Base */
     KC_EQL,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_GRAVE,KC_BSLS,KC_F14,
-    KC_BSPC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,         HMDIR,
-    KC_TAB,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                  KC_ENT,
+    KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,         HMDIR,
+    KC_LGUI,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                  KC_ENT,
     KC_LCTL,  _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_CAPS,XXXXXXX,
-    KC_ESC,   KC_LGUI, KC_LALT,          KC_LSFT,          MO(1),            KC_SPC,                    KC_RALT, KC_ESC,  KC_APP, KC_RCTL
+    KC_ESC,   KC_LGUI, KC_LALT,          MT(MOD_LSFT, KC_BSPC),     MO(1),            KC_SPC,           KC_RALT, KC_ESC,  KC_APP, KC_RCTL
   ),
   [_FN] = LAYOUT_all( /* FN */
     RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
-    _______, _______, _______, KC_PGUP, _______, _______, _______, _______, KC_UP,   _______, _______, _______, _______,          _______,
+    _______, _______, _______, KC_PGUP, _______, _______, _______, KC_BSPC, KC_UP,   KC_DEL,  _______, _______, _______,          _______,
     _______, _______, KC_HOME, KC_PGDN, KC_END,  _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,                   _______,
-    _______, _______, _______, _______, _______, DM_REC1, DM_RSTP, DM_PLY1, _______, _______, _______, _______,          _______, _______,
-    _______, _______, _______,          _______,          _______,          _______,                   _______, _______, _______, _______
+    KC_LSFT, _______, _______, _______, _______, DM_REC1, DM_RSTP, DM_PLY1, KC_ESC,  _______, _______, _______,          _______, _______,
+    _______, _______, _______,          KC_ESC,           _______,          KC_ENT,                    _______, _______, _______, _______
   )
 };
 
