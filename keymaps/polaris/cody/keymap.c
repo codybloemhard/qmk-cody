936afcb78d9c0b9e9f4e69394541671bdac3f93b
Cody Bloemhard <cblmhrd@live.nl>
2020-06-11 09:51:32 +0200

    incremental changes

--- keymaps/polaris/cody/keymap.c
+++ keymaps/polaris/cody/keymap.c
@@ -60,10 +60,10 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_all( /* Base */
     KC_EQL,                     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_GRAVE,KC_BSLS, KC_F14,
-    KC_ENT,                     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          HMDIR,
-    MT(MOD_LGUI, KC_TAB),       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ESC,
+    LT(L_MOUSE, KC_TAB),        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          HMDIR,
+    MT(MOD_LGUI, KC_ENT),       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ESC,
     MT(MOD_LCTL, KC_BSPC),      _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_CAPS, LROTATE,
-    MO(L_MOUSE),                KC_LGUI, KC_LALT,          KC_LSFT,          LT(L_FN, KC_ESC), KC_SPC,                    KC_RALT, KC_ENT,  KC_APP,  KC_RCTL
+    KC_LCTL,                    KC_LGUI, KC_LALT,          KC_LSFT,          LT(L_FN, KC_ESC), KC_SPC,                    KC_RALT, KC_ENT,  KC_APP,  KC_RCTL
   ),
   [_STANDARDISH] = LAYOUT_all( /* Standardish */
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_DEL,
@@ -81,9 +81,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_MOUSE] = LAYOUT_all( /* Mouse */
     _______,                    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______,                    _______, _______, _______, _______, _______, _______, KC_BTN1, KC_MS_U, KC_BTN2, _______, _______, _______,          _______,
+    _______,                    KC_ACL0, KC_ACL1, KC_ACL2, _______, _______, _______, KC_BTN1, KC_MS_U, KC_BTN2, _______, _______, _______,          _______,
     _______,                    _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______,                   _______,
-    _______,                    _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
+    _______,                    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
     _______,                    _______, _______,          _______,          _______,          _______,                   _______, _______, _______, _______
   ),
 };
