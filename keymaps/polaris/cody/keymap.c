b5d94d210f2566298d432fdd9f541d96b871329d
Cody Bloemhard <cblmhrd@live.nl>
2020-06-21 18:27:42 +0200

    small improvements with delete and alt

--- keymaps/polaris/cody/keymap.c
+++ keymaps/polaris/cody/keymap.c
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LT(L_MOUSE, KC_TAB),        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_EQL,
     MT(MOD_LGUI, KC_BSPC),      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_BSPC,
     MT(MOD_LCTL, KC_ESC),       _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_CAPS, LROTATE,
-    KC_LCTL,                    KC_LGUI, KC_LALT,          KC_LSFT,          LT(L_FN, KC_ENT), KC_SPC,                    KC_RALT, KC_ENT,  KC_APP,  KC_RCTL
+    KC_LALT,                     KC_LGUI, KC_LALT,          KC_LSFT,          LT(L_FN, KC_ENT), KC_SPC,                    KC_RALT, KC_ENT,  KC_APP,  KC_RCTL
   ),
   [_STANDARDISH] = LAYOUT_all( /* Standardish */
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_DEL,
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_FN] = LAYOUT_all( /* FN */
     RESET,                      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
     _______,                    _______, _______, KC_PGUP, _______, _______, _______, KC_BSPC, KC_UP,   KC_DEL,  _______, _______, _______,          _______,
-    _______,                    _______, KC_HOME, KC_PGDN, KC_END,  _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,                   _______,
+    _______,                    _______, KC_HOME, KC_PGDN, KC_END,  _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,                   KC_DEL,
     KC_LSFT,                    _______, _______, DM_RSTP, DM_REC1, DM_PLY1, DM_REC2, DM_PLY2, KC_ESC,  _______, _______, _______,          _______, _______,
     _______,                    _______, _______,          KC_ESC,           _______,          KC_ENT,                    _______, _______, _______, _______
   ),
