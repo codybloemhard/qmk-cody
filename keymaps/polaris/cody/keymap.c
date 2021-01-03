20dc63a656573e2cad41c5e332add9189808bb52
Cody Bloemhard <cblmhrd@live.nl>
2021-01-03 22:39:21 +0100

    separate rotating base layer and printing baselayer

--- keymaps/polaris/cody/keymap.c
+++ keymaps/polaris/cody/keymap.c
@@ -18,6 +18,7 @@
 enum custom_keycodes{
     HMDIR,
     LROTATE,
+    LPRINT,
 };
 
 char bfl = 0; // base layer, used to rotate bewteen bottom layer
@@ -34,11 +35,11 @@ char nbls = 3;
 // Build from a modified branch
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
+        // Immediately select the hold action when another key is pressed.
         case LT(L_FN, KC_ENT):
-            // Immediately select the hold action when another key is pressed.
             return true;
+        // Do not select the hold action when another key is pressed.
         default:
-            // Do not select the hold action when another key is pressed.
             return false;
     }
 }
@@ -55,18 +56,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record){
                     layer_on(bfl);
                     layer_off(L_BASE_GAMING);
                     layer_off(L_BASE_STDISH);
-                    SEND_STRING("#baselayer activated: cody-qgmlwy\n");
                 }else if(bfl == L_BASE_GAMING){
                     layer_on(bfl);
                     layer_off(L_BASE_CODY);
                     layer_off(L_BASE_STDISH);
-                    SEND_STRING("#baselayer activated: gaming-qgmlwy\n");
                 }else if(bfl == L_BASE_STDISH){
                     layer_on(bfl);
                     layer_off(L_BASE_CODY);
                     layer_off(L_BASE_GAMING);
-                    SEND_STRING("#baselayer activated: standardish-qwerty\n");
                 }
+            }
+            break;
+        case LPRINT:
+            if(record->event.pressed){
+                if(bfl == L_BASE_CODY)
+                    SEND_STRING("#baselayer: cody-qgmlwy\n");
+                else if(bfl == L_BASE_GAMING)
+                    SEND_STRING("#baselayer: gaming-qgmlwy\n");
+                else if(bfl == L_BASE_STDISH)
+                    SEND_STRING("#baselayer: standardish-qwerty\n");
             }
             break;
     }
@@ -85,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_all( /* QGMLWY base layer */
     KC_BSLS,                    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_GRAVE,HMDIR,   KC_F13,
     LT(L_MOUSE, KC_TAB),        KC_Q,    KC_G,    KC_M,    KC_L,    KC_W,    KC_Y,    KC_F,    KC_U,    KC_B,    KC_SCLN, KC_LBRC, KC_RBRC,          KC_EQL,
-    MT(MOD_LGUI, KC_BSPC),      KC_D,    KC_S,    KC_T,    KC_N,    KC_R,    KC_I,    KC_A,    KC_E,    KC_O,    KC_H,    KC_QUOT,                   KC_BSPC,
+    MT(MOD_LGUI, KC_DEL),       KC_D,    KC_S,    KC_T,    KC_N,    KC_R,    KC_I,    KC_A,    KC_E,    KC_O,    KC_H,    KC_QUOT,                   KC_BSPC,
     MT(MOD_LCTL, KC_ESC),       _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_J,    KC_K,    KC_P,    KC_COMM, KC_DOT,  KC_SLSH,          KC_CAPS, LROTATE,
     KC_LALT,                    KC_LGUI, KC_LALT,          KC_LSFT,          LT(L_FN, KC_ENT), KC_SPC,                    KC_RALT, KC_ENT,  KC_APP,  KC_RCTL
   ),
@@ -107,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RESET,                      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
     _______,                    _______, _______, KC_PGUP, _______, _______, _______, KC_BSPC, KC_UP,   KC_DEL,  _______, _______, _______,          _______,
     _______,                    _______, KC_HOME, KC_PGDN, KC_END,  _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,                   KC_DEL,
-    KC_LSFT,                    _______, _______, DM_RSTP, DM_REC1, DM_PLY1, DM_REC2, DM_PLY2, KC_ESC,  _______, _______, _______,          _______, _______,
+    KC_LSFT,                    _______, _______, DM_RSTP, DM_REC1, DM_PLY1, DM_REC2, DM_PLY2, KC_ESC,  _______, _______, _______,          _______, LPRINT,
     _______,                    _______, _______,          KC_ESC,           _______,          KC_ENT,                    _______, _______, _______, _______
   ),
   [_MOUSE] = LAYOUT_all( /* Mouse */
